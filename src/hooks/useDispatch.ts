ca70f2c9e6fa814423303f5d561b655c244e19a5
Francisco Veracoechea <francisco@webstudioswest.com>
2022-12-06 19:01:55 -0500

    updating project setup

--- src/hooks/useDispatch.ts
+++
@@ -1,9 +0,0 @@
-import useStore from './useStore';
-
-const useDispatch = () => {
-  const store = useStore();
-
-  return store.dispatch;
-};
-
-export default useDispatch;
--- src/store/Context.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Store } from '../types';
-
-export const StoreContext = React.createContext<Store | null>(null);
